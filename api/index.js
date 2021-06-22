fb8730e1bb825cba150bf006cb8b28f28decda70
AlexeiMir <alex_16.07@mail.ru>
2021-06-22 14:05:02 +0700

    изменил на https пост запрос

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ export class PlayerApi{
         return fetch('https://reactmarathon-api.herokuapp.com/api/mk/player/choose').then(res => res.json())
     }
     postFight(hit, defence){
-        const result = fetch('http://reactmarathon-api.herokuapp.com/api/mk/player/fight', {
+        const result = fetch('https://reactmarathon-api.herokuapp.com/api/mk/player/fight', {
             method: 'POST',
             body: JSON.stringify({
                 hit,
